178c848adbf9217143cb405d7cbff83950741df2
Mannyy21 <tom.etaixs@gmail.com>
2022-03-18 10:06:46 +0100

    Ajout estPAir
    Répond par "Vrai" si pair.
    "Faux" sinon.

--- TD_GitHub[1].cpp
+++ TD_GitHub[1].cpp
@@ -10,14 +10,17 @@
 
 
 //Fonctions réalisées par l'étudiant 2 :
-
-
-
-
-
-
-
-
-
+bool estPair(int nb)
+{
+	if (nb%2==0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+	
 
 //Fonctions réalisées par l'étudiant 3 :
