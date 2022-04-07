d035df98a264607bf60b20abba6ef84e39e32915
CorentinCollas <corentincollas24@gmail.com>
2022-04-07 08:24:29 +0200

    Update TD_GitHub[1].cpp

--- TD_GitHub[1].cpp
+++ TD_GitHub[1].cpp
@@ -99,3 +99,47 @@ int plusPetit(int a, int b, int c)
 }
 
 //Fonctions réalisées par l'étudiant 3 :
+
+bool estImpair(int nb)
+{
+	if (nb%2==0)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+
+float moyenne(int nb1, int nb2, int nb3)
+{
+    float resultat ;
+    resultat = (nb1 + nb2 +nb3)/ 3 ;
+    return resultat ;
+}
+
+float reduction(int prix, int reduc)
+{
+    float resultat ;
+    resultat = prix*(1-(reduc/100));
+    return resultat ;
+}
+
+int nombreMedian(int a, int b, int c)
+{
+  if ((a>=b && a<=c )||(a>=c && a<=b))
+  {
+      return a ;
+  }
+  if ((b>=a && b<=c)||(b>=c && b<=a))
+  {
+      return b ;
+  }
+  if ((c>=a && c<=b)||(c>=b && c<=a))
+  {
+      return c ;
+  }
+return 0 ;
+}
